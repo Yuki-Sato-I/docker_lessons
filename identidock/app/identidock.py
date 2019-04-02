1b2bea8e5e189a2bc2d1732dc6f6a379b1a605f6
YukiSato <yukiyukiamam@gmail.com>
2019-04-02 11:38:08 +0900

    第6章終了

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
@@ -19,7 +21,7 @@ def mainpage():
 
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-              Hello<input type="text" name="name" value="{}">
+              Hello!!!!<input type="text" name="name" value="{}">
               <input type="submit" value="submit">
               </form>
               <p>You look like a:
@@ -31,8 +33,12 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size/80')
-    image = r.content
+    image = cache.get(name)
+    if image is None:
+        print ("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size/80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
