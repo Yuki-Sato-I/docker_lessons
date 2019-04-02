d984088ec761afae1c57585f1b0aeda014784a27
YukiSato <yukiyukiamam@gmail.com>
2019-04-02 16:03:53 +0900

    第8章はjenkinsの仕様が変わったため再現不可

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
@@ -13,12 +14,10 @@ def mainpage():
 
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True)
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
-
-
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
               Hello!!!!<input type="text" name="name" value="{}">
@@ -33,6 +32,8 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
+
+    name = html.escape(name, quote=True)
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
